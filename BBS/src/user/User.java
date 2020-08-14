344554a5f6ebb497271ce86f2a2e050549627843
manggong <jihwank1995@gmail.com>
2020-08-14 12:56:21 +0900

    Add java beans

---
+++ BBS/src/user/User.java
@@ -0,0 +1,10 @@
+package user;
+
+public class User {
+	
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+}
